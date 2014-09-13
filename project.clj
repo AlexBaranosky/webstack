ea57190ca440a4328d63d19fecf6fce5bf3351c3
Alex Baranosky <alexander.baranosky@gmail.com>
2014-09-13 04:09:32 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject webstack "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clout "1.2.0"]
+                 [liberator "0.12.1"]
+                 [org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojars.runa/kits "1.19.25"]
+                 [stencil "0.3.4"]])
---
+++ src/webstack/core.clj
@@ -0,0 +1,62 @@
+(ns webstack.core
+  (:require [clout.core :as clout]
+            [kits.clout :as kc]
+            [liberator.core :as lib]
+            [stencil.core :as stenc]))
+
+(defn- render-template [template data-map]
+  (stenc/render-file (str "templates/" "page.mustache") data-map))
+
+(lib/defresource ping
+  :available-media-types ["text/plain"]
+  :handle-ok "pong")
+
+(lib/defresource home-page
+  :allowed-methods [:get]
+  :available-media-types ["text/html"]
+  :handle-ok (fn [ctx]
+               (render-template "page.mustache"
+                                {:content (-> ctx :request :params :msg)})))
+
+(defonce comments-atom (atom {}))
+
+(defn- param [ctx k]
+  (-> ctx :request :params k))
+
+(lib/defresource comments
+  :allowed-methods [:get :post :put :delete]
+  :available-media-types ["application/json"]
+  :malformed? (fn [_] false)
+  :post! (fn [ctx]
+           (swap! comments-atom assoc (param ctx :id) (param ctx :comment)))
+  :put! #(%)
+  :delete! #(%)
+  :handle-ok ::comment)
+;; SEE: http://clojure-liberator.github.io/liberator/tutorial/all-together.html
+
+(def ^:private r clout/route-compile)
+
+(def ^:private routes
+  {(r "/")                   home-page
+   (r "/ping")               ping
+   (r "/resources/comments") comments})
+
+(defn handler [req]
+  (if-let [{hdlr :handler
+            params :params} (kc/find-matching-handler routes (:uri req))]
+    (hdlr (update-in req [:params] conj params))
+    (comment 'do-something)))
+
+(comment
+  (handler {:uri "/"
+            :params {:msg "Welcome Alex!"}
+            :request-method :get})
+
+  (handler {:uri "/ping"
+            :request-method :get})
+
+  (handler {:uri "/resources/comments"
+            :params {:id "comment-1"
+                     :comment "hi!"}
+            :request-method :post})
+  )
