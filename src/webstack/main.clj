388e64ec9c255b7d22ef02970cc6c27368809a51
Alex Baranosky <alexander.baranosky@gmail.com>
2014-09-14 18:45:45 -0700

    improve server startup logging

--- src/webstack/main.clj
+++ src/webstack/main.clj
@@ -5,7 +5,7 @@
   (:gen-class))
 
 (defn -main [& args]
-  (println (str "Starting server on port " 9445))
+  (println (str "Starting nREPL server on port " 9445))
   (nrepl/start-server :port 9445)
-  (println (str "Starting server on port " 9444))
+  (println (str "Starting Jetty server on port " 9444))
   (server/start 9444))
