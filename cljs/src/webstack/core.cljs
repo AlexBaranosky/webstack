29bbc12f7e55a3dc3d0a1b98c9d298705f552e08
Alex Baranosky <alexander.baranosky@gmail.com>
2014-09-14 21:02:58 -0700

    WIP: weasel

--- cljs/src/webstack/core.cljs
+++ cljs/src/webstack/core.cljs
@@ -1,6 +1,9 @@
 (ns webstack.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [weasel.repl :as ws-repl]))
+
+(ws-repl/connect "ws://localhost:9001" :verbose true)
 
 (defn widget [data]
   (om/component
