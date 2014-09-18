e582921a45e574748865bbccacbf033ec72e9780
Alex Baranosky <alexander.baranosky@gmail.com>
2014-09-17 18:55:24 -0700

    test cleansup database

--- test/webstack/crud_test.clj
+++ test/webstack/crud_test.clj
@@ -1,30 +1,37 @@
 (ns webstack.crud-test
   (:require [cheshire.core :as json]
             [clj-http.client :as client]
-            [clojure.test :refer :all]))
+            [clojure.java.jdbc :as jdbc]
+            [clojure.test :refer :all]
+            [webstack.resource-registrar :as resource-registrar]))
+
+(defmacro with-clean-db [tables & body]
+  `(do
+     (doseq [t# ~tables]
+       (jdbc/execute! resource-registrar/db [(str "DELETE FROM " t#)]))
+     ~@body))
 
 (defmacro def-crud-resource-test
   [name {:keys [url create]}]
   `(deftest ~(symbol (str "test-" name))
-     (testing ~(str "Create " name)
-       (is (= 201 (:status
-                   (client/post ~url
-                                {:body (json/encode {:id "4"
-                                                     :value ~create})
-                                 :content-type :json})))))
+     (with-clean-db [~(str name)]
+       (testing ~(str "Create " name)
+         (is (= 201 (:status
+                     (client/post ~url
+                                  {:body (json/encode {:id "2"
+                                                       :value ~create})
+                                   :content-type :json})))))
 
-     (testing ~(str "Read " name)
-       (is (= (assoc ~create :id 4)
-              (-> (client/get (str ~url "/?id=4"))
-                  :body
-                  (json/decode keyword)))))))
+       (testing ~(str "Read " name)
+         (is (= (assoc ~create :id 2)
+                (-> (client/get (str ~url "/?id=2"))
+                    :body
+                    (json/decode keyword))))))))
 
 
 (def-crud-resource-test comment
   {:url "http://localhost:9444/resources/comment"
-   :create {:text "My comment"}})
-
-
+   :create {:text "My comment"}}) 
 
 (comment
   (client/get "http://localhost:9444/resources/comment?id=1")
